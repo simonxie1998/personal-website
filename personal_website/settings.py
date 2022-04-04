df232b9b354263a242246f718f3bf872246504c6
Simon Xie <simonxie98@utexas.edu>
2022-04-03 20:27:00 -0700

    fix typo

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.jojin(BASE_DIR, "static")
+STATIC_ROOT = os.path.jojin(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 
 # Default primary key field type
